bc0e3fc5cc26aad988532d4d1e630a71b2beee39
mayurpandit25 <panditmayur888@gmail.com>
2026-09-07 12:37:41 +0530

    terraform tfvars file

--- day-5-terraform-var-file/main.tf
+++ day-5-terraform-var-file/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
     source = "./module/vpc"
     vpc_cidr = var.vpc_cidr
-    public_subnet_cidr = var.private_subnet_cidr
+    public_subnet_cidr = var.public_subnet_cidr
     private_subnet_cidr = var.private_subnet_cidr
     public_az = var.public_az
     private_az = var.private_az
@@ -12,9 +12,9 @@ module "vpc" {
 
 module "ec2" {
     source = "./module/ec2"
-    ami = ""
-    instance_type = "t3.micro"
-    key_name = "abhi"
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
     public_subnet_id = module.vpc.public_subnet_id
     private_subnet_id =  module.vpc.private_subnet_id
     sg_id = module.vpc.sg_id
